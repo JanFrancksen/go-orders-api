b74fd16af1e75c9fc98b94fe164498dbea949ec1
j1n.dev <jan.francksen@gmail.com>
2023-11-12 22:40:01 +0100

    feat: add graceful shutdown

--- main.go
+++ main.go
@@ -1,29 +1,22 @@
 package main
 
 import (
-    "fmt"
-    "net/http"
-    "github.com/go-chi/chi/v5"
-    "github.com/go-chi/chi/v5/middleware"
+	"context"
+	"fmt"
+	"os"
+	"os/signal"
+
+	"github.com/janfrancksen/orders-api/application"
 )
 
 func main() {
-    router := chi.NewRouter()
-    router.Use(middleware.Logger)
+	app := application.New()
 
-    router.Get("/hello", basicHandler)
+	ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt)
+	defer cancel()
 
-    server := &http.Server{
-        Addr: ":3000",
-        Handler: router,
-    }
-
-    err := server.ListenAndServe()
-    if err != nil {
-        fmt.Println("failed to listen to server", err)
-    }
-}
-
-func basicHandler(w http.ResponseWriter, r *http.Request) {
-    w.Write([]byte("Hello, world"))
+	err := app.Start(ctx)
+	if err != nil {
+		fmt.Println("failed to start app:", err)
+	}
 }
